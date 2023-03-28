4986e2330caabfd5dce1b20a6dda50255686a085
Juan <jaguirreb@fi.uba.ar>
2023-03-28 11:31:25 -0300

    Basic setup of Postgre db

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from app.api import ping
+from app.db import engine, db, metadata
+
+metadata.create_all(engine)
 
 app = FastAPI()
 
+@app.on_event("startup")
+async def startup():
+    await db.connect()
 
-@app.get("/ping")
-async def root():
-    return {"ping": "pong"}
+@app.on_event("shutdown")
+async def shutdown():
+    await db.disconnect()    
+    
+app.include_router(ping.router)
