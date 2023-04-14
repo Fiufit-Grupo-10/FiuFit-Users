f94936d5a6a7b6d242ecef532b000a421aee663d
axelkelman <akelman@fi.uba.ar>
2023-04-14 20:37:59 +0000

    fix

--- app/sql_app/database.py
+++ app/sql_app/database.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL,pool_pre_ping = True)
 # engine.execute(CreateSchema('users'))
 metadata = MetaData(schema="users")
 
