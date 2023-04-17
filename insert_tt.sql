46f14827fe64f12f1be394554fe20fb0ea5d8bd7
axelkelman <akelman@fi.uba.ar>
2023-04-17 02:40:48 -0300

    qf

---
+++ insert_tt.sql
@@ -0,0 +1,13 @@
+INSERT INTO trainingtypes VALUES 
+('Cardio','Entrenamientos relacionados a la resistencia aerobica'), 
+('Fuerza','Entrenamientos relacionados a ganar fuerza'), 
+('HIIT','Entrenamientos HIIT'),
+ ('Tonificación','Entrenamientos relacionados a tonificar los músculos'), 
+ ('Baile','Entrenamientos con musica'),
+ ('Kickboxing','Entrenamientos de pelea'), 
+ ('Pilates','Entrenamientos de pilates'),
+ ('Mediatación','Entrenamientos de meditación'),
+ ('Estirar','Entrenamientos de estiramiento'),
+ ('Yoga','Entrenamientos de Yoga'),
+ ('Spinning','Entrenamientos de spinning'),
+ ('Cinta','Entrenamientos de cinta');
