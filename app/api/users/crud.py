bbf40e67b30e4ec2767e8033af6bb6d246fdaabd
Juan <jaguirreb@fi.uba.ar>
2023-05-14 23:51:15 -0300

    flake

--- app/api/users/crud.py
+++ app/api/users/crud.py
@@ -30,7 +30,7 @@ def add_user_follower(
         db.refresh(new_following_relationship)
         return new_following_relationship
     # Chequear el error cuando es repetido
-    except IntegrityError as _:
+    except IntegrityError:
         follower_uid = follower.follower_uid
         detail = f"user with uid: {followed_uid},{follower_uid} does not exist"
         raise HTTPException(status_code=404, detail=detail)
