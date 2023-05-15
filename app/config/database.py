c0d9f1579b420538a79189dc0b64dbf4e2f285bc
Juan <jaguirreb@fi.uba.ar>
2023-05-15 00:01:28 -0300

    black fmt

--- app/config/database.py
+++ app/config/database.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import sessionmaker
 
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL, pool_pre_ping=True,connect_args={'sslmode':'require'},
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={"sslmode": "require"},
 )
 # engine.execute(CreateSchema('users'))
 metadata = MetaData(schema="users")
